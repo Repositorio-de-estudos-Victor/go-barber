74912320a81c8ec7ecdd433b09057d49d8a2b3a3
Victor Novais <victornovais@MacBook-Air-de-Victor.local>
2019-10-15 23:06:52 -0300

    Criação das migrations

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '@vic5922',
+  database: 'goBarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
