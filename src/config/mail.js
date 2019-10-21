0ce15ee4c8354c34f54d2c06cdd50b3efadd7f28
Victor Novais <victornovais@MacBook-Air-de-Victor.local>
2019-10-21 08:44:56 -0300

    Criação de Notifications e configuração do mongo

---
+++ src/config/mail.js
@@ -0,0 +1,20 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '523039e480caad',
+    pass: '001502b12402b5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Amazon SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ *
+ */
